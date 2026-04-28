3d7aa587642d700eea5d805a169442a6a2291164
Daniel Picconi <picconidaniel@gmail.com>
2026-04-28 14:42:11 -0300

    Implementação Parcial da Semana 2

--- src/trace_runtime.c
+++ src/trace_runtime.c
@@ -52,8 +52,29 @@ static pid_t launch_tracee(char *const argv[])
      *
      * Em erro, imprima uma mensagem com perror() e retorne -1.
      */
+    
     pid_t pid = fork();
-    fprintf(stderr, "erro: TODO Semana 2: implementar launch_tracee()\n");
+    // Entra na condicional se o processo for o filho
+    if (pid == 0) {
+        // Filho notifica ao kernel que será rastreado pelo pai
+        if (ptrace(PTRACE_TRACEME, 0, NULL, NULL) == -1) {
+            perror("ptrace TRACEME");
+            _exit(1);
+        }
+        // Para imediatamente para o pai fazer o waitpid inicial
+        if (raise(SIGSTOP) != 0) {
+            perror("raise SIGSTOP");
+            _exit(1);
+        }
+        // Executa o programa requisitado
+        execvp(argv[0], argv);
+        // Se o execvp falhar, exibe o erro
+        perror("execvp falhou!");
+        _exit(1);
+    }else if (pid > 0) {
+        return pid;
+    }else
+        perror("Erro no fork");
     return -1;
 }
 
@@ -67,8 +88,7 @@ static int wait_for_initial_stop(pid_t child)
      *
      * Retorne 0 se o filho parou como esperado, -1 em erro.
      */
-    fprintf(stderr, "erro: TODO Semana 2: implementar wait_for_initial_stop()\n");
-    return -1;
+    return 0;
 }
 
 static int configure_trace_options(pid_t child)
